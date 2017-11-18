33676d3d6caa8f82b7d45e0280b7304d1c5a4e03
Danilich <logolevel@gmail.com>
2017-11-18 23:31:53 +0200

    responsive multi steps

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
 $(function() {
 
-	// Custom JS
+	/*progress-nav*/
+	$('.progress-nav__dot').on('click', function(){
+		if ( $(this).hasClass('active') ) {
+			$(this)
+				.removeClass('success')
+				.nextAll('.progress-nav__dot')
+				.removeClass('active success');
+			$(this)
+				.nextAll('.progress-nav__line')
+				.removeClass('success');
+
+		} else {
+			$(this)
+				.addClass('active')
+				.prevAll('.progress-nav__dot')
+				.addClass('active success');
+			$(this)
+				.prevAll('.progress-nav__line')
+				.addClass('success');
+		}
+	});
 
 });
